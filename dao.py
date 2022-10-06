c2aa9aa60f456ef6090f570d26a9bf2f08e1f3ac
vlddev <volodymyr.vlad@yahoo.com>
2022-10-06 12:20:04 +0200

    DAO objects

---
+++ dao.py
@@ -0,0 +1,42 @@
+
+
+class Author:
+    def __init__(self, dataDict):
+        # TODO fill from data, data is a map or named params???
+        self.id = dataDict["id"]
+        self.name = dataDict["name"]
+
+class AuthorName:
+    def __init__(self, dataDict):
+        self.id = dataDict["id"]
+        self.author_id = dataDict["author_id"]
+        self.name = dataDict["name"]
+        self.lang = dataDict["lang"]
+        self.type = dataDict["type"]
+
+class Book:
+    def __init__(self, dataDict):
+        self.id = dataDict["id"]
+        self.title = dataDict["title"]
+        self.genre = dataDict["genre"]
+        self.note = dataDict["note"]
+        self.lang = dataDict["lang"]
+        self.publish_date = dataDict["publish_date"]
+
+class BookName:
+    def __init__(self, dataDict):
+        self.id = dataDict["id"]
+        self.book_id = dataDict["book_id"]
+        self.name = dataDict["name"]
+        self.lang = dataDict["lang"]
+
+class ReadedBook:
+    def __init__(self, dataDict):
+        self.id = dataDict["id"]
+        self.book_id = dataDict["book_id"]
+        self.medium = dataDict["medium"]
+        self.score = dataDict["score"]
+        self.note = dataDict["note"]
+        self.lang_read = dataDict["lang_read"]
+        self.date_read = dataDict["date_read"]
+
